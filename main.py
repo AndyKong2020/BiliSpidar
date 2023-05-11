e51b18f98e8821096f6a1b88f6f83cf5edf569a1
AndyKong <andy.kong@nuaa.edu.cn>
2023-05-11 19:55:57 +0800

    [feat]增加使用正则的过滤器

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from utils.bilibili_spider import Bilibili_Spider
 
 
 def main(args):
-    bilibili_spider = Bilibili_Spider(args.uid, args.save_dir, args.save_by_page, args.time)
+    filter_cfg = {'title': '南京航空航天大学'}
+    bilibili_spider = Bilibili_Spider(args.uid, args.save_dir, args.save_by_page, args.time, filter_cfg)
     bilibili_spider.get()
     if args.detailed:
         bilibili_spider.get_detail()
@@ -14,7 +15,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--uid', type=str, default='362548791')
+    parser.add_argument('--uid', type=str, default='20554233')
     parser.add_argument('--save_dir', type=str, default='json')
     parser.add_argument('--save_by_page', action='store_true', default=False)
     parser.add_argument('--time', type=int, default=2, help='waiting time for browser.get(url) by seconds')
